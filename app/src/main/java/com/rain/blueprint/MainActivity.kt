815887e49d7104912152b839e90859e1d834c7d6
steamyrain <reinhard.aditya@gmail.com>
2020-09-20 10:22:52 +0700

    Added NavigationUI to action bar

--- app/src/main/java/com/rain/blueprint/MainActivity.kt
+++ app/src/main/java/com/rain/blueprint/MainActivity.kt
@@ -2,11 +2,29 @@ package com.rain.blueprint
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    val navController: NavController by lazy {
+        findNavController(R.id.nav_host_fragment)
+    }
+
+    val appBarConfiguration by lazy {
+        AppBarConfiguration(navController.graph)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
+
